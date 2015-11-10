2084e227861252bd1130675a8df2b57def76683a
Miquel Perelló Nieto <perello.nieto@gmail.com>
2015-11-10 16:44:36 +0000

    Output only the layers that are known to contain a blow

--- cifarcatdog/experiments/cat_vs_dog/caffe_get_blobs.py
+++ cifarcatdog/experiments/cat_vs_dog/caffe_get_blobs.py
@@ -7,6 +7,9 @@ from caffe.proto import caffe_pb2
 from google.protobuf import text_format
 from math import ceil
 
+LAYERS_WITH_BLOB = ['Convolution', 'Pooling', 'Concat', 'InnerProduct', 'LRN',
+                    'Accuracy', 'Softmax', 'SoftmaxWithLoss']
+
 def get_pooling_types_dict():
     """Get dictionary mapping pooling type number to type name
     """
@@ -20,46 +23,47 @@ def get_pooling_types_dict():
 def print_blob_names(net, params=True):
     separator = " "
     for layer in net.layer:
-        if params:
-            if layer.type == 'Convolution' or layer.type == 'Deconvolution':
-                k = layer.convolution_param.kernel_size[0] if len(layer.convolution_param.kernel_size._values) else 1
-                s = layer.convolution_param.stride[0] if len(layer.convolution_param.stride._values) else 1
-                p = layer.convolution_param.pad[0] if len(layer.convolution_param.pad._values) else 0
-                blob_size = ceil((2*p-k)/s)+1
-                node_label = '%s%s(%s)%sk: %d%ss: %d%sp: %d = %s' %\
-                             (layer.name,
-                              separator,
-                              layer.type,
-                              separator,
-                              layer.convolution_param.kernel_size[0] if len(layer.convolution_param.kernel_size._values) else 1,
-                              separator,
-                              layer.convolution_param.stride[0] if len(layer.convolution_param.stride._values) else 1,
-                              separator,
-                              layer.convolution_param.pad[0] if len(layer.convolution_param.pad._values) else 0,
-                              blob_size)
-            elif layer.type == 'Pooling':
-                pooling_types_dict = get_pooling_types_dict()
-                k = layer.pooling_param.kernel_size
-                s = layer.pooling_param.stride
-                p = layer.pooling_param.pad
-                blob_size = ceil((2*p-k)/s)+1
-                node_label = '%s%s(%s %s)%skernel size: %d%sstride: %d%spad: %d = %s' %\
-                             (layer.name,
-                              separator,
-                              pooling_types_dict[layer.pooling_param.pool],
-                              layer.type,
-                              separator,
-                              layer.pooling_param.kernel_size,
-                              separator,
-                              layer.pooling_param.stride,
-                              separator,
-                              layer.pooling_param.pad,
-                              blob_size)
+        if layer.type in LAYERS_WITH_BLOB:
+            if params:
+                if layer.type == 'Convolution' or layer.type == 'Deconvolution':
+                    k = layer.convolution_param.kernel_size[0] if len(layer.convolution_param.kernel_size._values) else 1
+                    s = layer.convolution_param.stride[0] if len(layer.convolution_param.stride._values) else 1
+                    p = layer.convolution_param.pad[0] if len(layer.convolution_param.pad._values) else 0
+                    blob_size = ceil((2*p-k)/s)+1
+                    node_label = '%s%s(%s)%sk: %d%ss: %d%sp: %d = %s' %\
+                                 (layer.name,
+                                  separator,
+                                  layer.type,
+                                  separator,
+                                  layer.convolution_param.kernel_size[0] if len(layer.convolution_param.kernel_size._values) else 1,
+                                  separator,
+                                  layer.convolution_param.stride[0] if len(layer.convolution_param.stride._values) else 1,
+                                  separator,
+                                  layer.convolution_param.pad[0] if len(layer.convolution_param.pad._values) else 0,
+                                  blob_size)
+                elif layer.type == 'Pooling':
+                    pooling_types_dict = get_pooling_types_dict()
+                    k = layer.pooling_param.kernel_size
+                    s = layer.pooling_param.stride
+                    p = layer.pooling_param.pad
+                    blob_size = ceil((2*p-k)/s)+1
+                    node_label = '%s%s(%s %s)%skernel size: %d%sstride: %d%spad: %d = %s' %\
+                                 (layer.name,
+                                  separator,
+                                  pooling_types_dict[layer.pooling_param.pool],
+                                  layer.type,
+                                  separator,
+                                  layer.pooling_param.kernel_size,
+                                  separator,
+                                  layer.pooling_param.stride,
+                                  separator,
+                                  layer.pooling_param.pad,
+                                  blob_size)
+                else:
+                    node_label = '%s%s(%s)' % (layer.name, separator, layer.type)
+                print node_label
             else:
-                node_label = '%s%s(%s)' % (layer.name, separator, layer.type)
-            print node_label
-        else:
-            print layer.name
+                print layer.name
 
 def parse_args():
     """Parse input arguments
